86b1a6ba097c50428a99b46e2a0f7a2ff7f0c152
Quantum Coding <166281221+QuantumCoding123@users.noreply.github.com>
2024-04-25 10:46:19 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+    /* 
+   MixitUp
+   ========================================================================== */
+  $('#portfolio').mixItUp();
+
+  /* 
+   One Page Navigation & wow js
+   ========================================================================== */
+    var OnePNav = $('.onepage-nev');
+    var top_offset = OnePNav.height() - -0;
+    OnePNav.onePageNav({
+      currentClass: 'active',
+      scrollOffset: top_offset,
+    });
+  
+  /*Page Loader active
+    ========================================================*/
+	
+		/*PRELOADER JS*/ 
+			$('.spinner').fadeOut();
+			$('.preloader').delay(350).fadeOut('slow'); 
+		/*END PRELOADER JS*/
+
+		/*START MENU JS*/		
+			 if ($(window).scrollTop() > 200) {
+              $('.fixed-top').addClass('menu-bg');
+          } else {
+              $('.fixed-top').removeClass('menu-bg');
+          }
+			$(window).on('scroll', function(){
+				if ( $(window).scrollTop() > 100 ) {
+					$('.site-navigation, .header-white, .header').addClass('navbar-fixed');
+				} else {
+					$('.site-navigation, .header-white, .header').removeClass('navbar-fixed');
+				}
+			});				
+		/*END MENU JS*/	
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+
+    wow.init();
+
+    /* Nivo Lightbox 
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+      });
+
+    /* Counter
+    ========================================================*/
+    $('.counterUp').counterUp({
+     delay: 20,
+     time: 3000
+    });
+	
+	 /* Tesimonial
+    ========================================================*/
+	$(".carousel-testimony").owlCarousel({
+		loop:true,
+		navText:false,
+		autoplay: true,
+		items:1,
+		slideSpeed: 5000,
+		responsive:{
+			0:{
+				items:1
+			},
+			768:{
+				items:1
+			},
+			979:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+
+
+  });      
+
+}(jQuery));
---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$from = 'Parax Contact Form'; 
+		$to = 'example@gmail.com'; 
+		$subject = $_POST['subject']; 
+		
+		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+	
+
+	mail($to, $subject, $body, $from) or die("Error!");
+
+	header("location: thank-you.html");
+	
+	}
+	
+?>
